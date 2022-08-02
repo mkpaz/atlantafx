a0a5f0ca85f9bca9a4fbd41afa8683d2605001d2
Florian Enner <florian@hebirobotics.com>
2022-08-02 10:46:54 +0200

    Replace maven-frontend-plugin with sass-cli-maven-plugin
    Use Maven wrapper around Dart SASS binary instead of downloading and installing NodeJS / NPM locally. This also eliminates dependency from Grunt and its modules.

--- styles/compile-scss-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-# This scipt compiles SCSS directly to the sampler classpath for hot reload (CSSFX).
-
-PATH="../node:$(../node/npm bin):$PATH"
-NODE_ENV=dev grunt --verbose --gruntfile=Gruntfile.js
