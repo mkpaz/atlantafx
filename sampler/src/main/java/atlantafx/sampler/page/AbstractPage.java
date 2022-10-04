6b2216aba06b21869741ebc5c4c48d388afe9f95
CodeDead <admin@codedead.com>
2022-10-04 20:33:06 +0200

    Removed unused import

--- sampler/src/main/java/atlantafx/sampler/page/AbstractPage.java
+++ sampler/src/main/java/atlantafx/sampler/page/AbstractPage.java
@@ -12,7 +12,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static atlantafx.sampler.util.Containers.setScrollConstraints;
