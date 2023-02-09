69e521737c9fffd2da5fdcacf6c953bab913be1b
mkpaz <quizynox@gmail.com>
2023-02-09 15:28:24 +0400

    ErrorProne: fix sampler/EqualsGetClass

--- sampler/src/main/java/atlantafx/sampler/event/Event.java
+++ sampler/src/main/java/atlantafx/sampler/event/Event.java
@@ -16,8 +16,7 @@ public abstract class Event {
     @Override
     public boolean equals(Object o) {
         if (this == o) { return true; }
-        if (o == null || getClass() != o.getClass()) { return false; }
-        Event event = (Event) o;
+        if (!(o instanceof Event event)) { return false; }
         return id.equals(event.id);
     }
 
