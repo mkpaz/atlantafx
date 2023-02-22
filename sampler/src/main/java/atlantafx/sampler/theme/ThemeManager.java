318cc438946c07e556ff7ef66208547ced8ec089
mkpaz <quizynox@gmail.com>
2023-02-22 20:30:08 +0400

    Animate Sampler theme change

--- sampler/src/main/java/atlantafx/sampler/theme/ThemeManager.java
+++ sampler/src/main/java/atlantafx/sampler/theme/ThemeManager.java
@@ -29,10 +29,18 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.css.PseudoClass;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 public final class ThemeManager {
 
@@ -98,6 +106,10 @@ public final class ThemeManager {
      */
     public void setTheme(SamplerTheme theme) {
         Objects.requireNonNull(theme);
+
+        if (currentTheme != null) {
+            animateThemeChange(Duration.millis(750));
+        }
 
         Application.setUserAgentStylesheet(Objects.requireNonNull(theme.getUserAgentStylesheet()));
         getScene().getStylesheets().setAll(theme.getAllStylesheets());
@@ -183,6 +195,8 @@ public final class ThemeManager {
     public void setAccentColor(AccentColor color) {
         Objects.requireNonNull(color);
 
+        animateThemeChange(Duration.millis(350));
+
         if (accentColor != null) {
             getScene().getRoot().pseudoClassStateChanged(accentColor.pseudoClass(), false);
         }
@@ -194,6 +208,8 @@ public final class ThemeManager {
     }
 
     public void resetAccentColor() {
+        animateThemeChange(Duration.millis(350));
+
         if (accentColor != null) {
             getScene().getRoot().pseudoClassStateChanged(accentColor.pseudoClass(), false);
             accentColor = null;
@@ -266,6 +282,21 @@ public final class ThemeManager {
         } else {
             removeCustomDeclaration(colorName);
         }
+    }
+
+    private void animateThemeChange(Duration duration) {
+        Image snapshot = scene.snapshot(null);
+        Pane root = (Pane) scene.getRoot();
+
+        ImageView imageView = new ImageView(snapshot);
+        root.getChildren().add(imageView); // add snapshot on top
+
+        var transition = new Timeline(
+            new KeyFrame(Duration.ZERO, new KeyValue(imageView.opacityProperty(), 1, Interpolator.EASE_OUT)),
+            new KeyFrame(duration, new KeyValue(imageView.opacityProperty(), 0, Interpolator.EASE_OUT))
+        );
+        transition.setOnFinished(e -> root.getChildren().remove(imageView));
+        transition.play();
     }
 
     private void reloadCustomCSS() {
