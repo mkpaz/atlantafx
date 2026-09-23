44b19d91ca93ef5ccb2a7c72d912fe0f8d4b9a56
mkpaz <quizynox@gmail.com>
2026-09-23 22:29:19 +0400

    Update checkstyle

--- sampler/src/main/java/atlantafx/sampler/Launcher.java
+++ sampler/src/main/java/atlantafx/sampler/Launcher.java
@@ -38,25 +38,25 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Properties;
 
-import static java.nio.charset.StandardCharsets.*;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 // Sampler metadata: resources (practically everything including .java files and assets), and reflectively looked-up pages
 @Reachable(resources = "**")
 @ReachableFxResources({"**/*.fxml", "**/*.css"}) // capture anything referenced inside FXML and CSS
 @Reachable(memberAccess = MemberAccess.ALL_DECLARED_CONSTRUCTORS, classes = {
-        AccordionPage.class, BreadcrumbsPage.class, ButtonPage.class, CalendarPage.class, CardPage.class,
-        ChartPage.class, CheckBoxPage.class, ChoiceBoxPage.class, ColorPickerPage.class, ComboBoxPage.class,
-        ContextMenuPage.class, CustomTextFieldPage.class, DatePickerPage.class, DeckPanePage.class, DialogPage.class,
-        HtmlEditorPage.class, InputGroupPage.class, ListViewPage.class, MenuBarPage.class, MenuButtonPage.class,
-        MessagePage.class, ModalPanePage.class, NotificationPage.class, PaginationPage.class, PopoverPage.class,
-        ProgressIndicatorPage.class, RadioButtonPage.class, RichTextPage.class, ScrollPanePage.class,
-        SegmentedControlPage.class, SeparatorPage.class, SidebarPage.class, SliderPage.class, SpinnerPage.class,
-        SpinsPage.class, SplitPanePage.class, TabLinePage.class, TabPanePage.class, TableViewPage.class,
-        TextAreaPage.class, TextFieldPage.class, TilePage.class, TitledPanePage.class, ToggleButtonPage.class,
-        ToggleSwitchPage.class, ToolBarPage.class, TooltipPage.class, TreeTableViewPage.class, TreeViewPage.class,
-        AnimationsPage.class, BBCodePage.class, DecorationsPage.class, IconsPage.class, SelectableTextFlowPage.class,
-        ThemePage.class, TypographyPage.class, UtilsPage.class, BlueprintsPage.class, OverviewPage.class,
-        FileManagerPage.class, MusicPlayerPage.class
+    AccordionPage.class, BreadcrumbsPage.class, ButtonPage.class, CalendarPage.class, CardPage.class,
+    ChartPage.class, CheckBoxPage.class, ChoiceBoxPage.class, ColorPickerPage.class, ComboBoxPage.class,
+    ContextMenuPage.class, CustomTextFieldPage.class, DatePickerPage.class, DeckPanePage.class, DialogPage.class,
+    HtmlEditorPage.class, InputGroupPage.class, ListViewPage.class, MenuBarPage.class, MenuButtonPage.class,
+    MessagePage.class, ModalPanePage.class, NotificationPage.class, PaginationPage.class, PopoverPage.class,
+    ProgressIndicatorPage.class, RadioButtonPage.class, RichTextPage.class, ScrollPanePage.class,
+    SegmentedControlPage.class, SeparatorPage.class, SidebarPage.class, SliderPage.class, SpinnerPage.class,
+    SpinsPage.class, SplitPanePage.class, TabLinePage.class, TabPanePage.class, TableViewPage.class,
+    TextAreaPage.class, TextFieldPage.class, TilePage.class, TitledPanePage.class, ToggleButtonPage.class,
+    ToggleSwitchPage.class, ToolBarPage.class, TooltipPage.class, TreeTableViewPage.class, TreeViewPage.class,
+    AnimationsPage.class, BBCodePage.class, DecorationsPage.class, IconsPage.class, SelectableTextFlowPage.class,
+    ThemePage.class, TypographyPage.class, UtilsPage.class, BlueprintsPage.class, OverviewPage.class,
+    FileManagerPage.class, MusicPlayerPage.class
 })
 public class Launcher extends Application {
 
--- sampler/src/main/java/atlantafx/sampler/LibMetadata.java
+++ sampler/src/main/java/atlantafx/sampler/LibMetadata.java
@@ -12,17 +12,17 @@ import us.hebi.graalvm.reachability.annotations.Reachable;
 
 // Faker data generation
 @Reachable(condition = Faker.class,
-        classes = {Faker.class, Name.class, Code.class, Metric.class, Product.class, Job.class},
-        resources = {"/*.yml", "/en/**"}
+    classes = {Faker.class, Name.class, Code.class, Metric.class, Product.class, Job.class},
+    resources = {"/*.yml", "/en/**"}
 )
 
 // Lib: Ikonli icon fonts
 @Reachable(condition = org.kordamp.ikonli.javafx.IkonResolver.class,
-        classes = org.kordamp.ikonli.javafx.IkonResolver.class,
-        resources = {
-                "/META-INF/services/org.kordamp.ikonli.IkonHandler",
-                "/META-INF/resources/*/*/fonts/*.ttf",
-        }
+    classes = org.kordamp.ikonli.javafx.IkonResolver.class,
+    resources = {
+        "/META-INF/services/org.kordamp.ikonli.IkonHandler",
+        "/META-INF/resources/*/*/fonts/*.ttf",
+    }
 )
 
 // Lib: devtoolsfx (that needs access to pretty much everything though, so it's not really working)
@@ -30,102 +30,102 @@ import us.hebi.graalvm.reachability.annotations.Reachable;
 
 // AWT: musicplayer/Utils, layout/MainLayer
 @Reachable(condition = javafx.embed.swing.SwingFXUtils.class, jniAccessible = true,
-        classes = {
-                java.awt.GraphicsEnvironment.class,
-                java.awt.image.BufferedImage.class,
-                java.awt.image.ColorModel.class,
-                java.awt.image.Raster.class,
-                java.awt.image.SampleModel.class,
-                java.awt.image.SinglePixelPackedSampleModel.class,
-                java.lang.System.class
-        },
-        classNames = "sun.awt.image.IntegerComponentRaster",
-        memberAccess = {MemberAccess.ALL_DECLARED_FIELDS, MemberAccess.ALL_DECLARED_METHODS}
+    classes = {
+        java.awt.GraphicsEnvironment.class,
+        java.awt.image.BufferedImage.class,
+        java.awt.image.ColorModel.class,
+        java.awt.image.Raster.class,
+        java.awt.image.SampleModel.class,
+        java.awt.image.SinglePixelPackedSampleModel.class,
+        java.lang.System.class
+    },
+    classNames = "sun.awt.image.IntegerComponentRaster",
+    memberAccess = {MemberAccess.ALL_DECLARED_FIELDS, MemberAccess.ALL_DECLARED_METHODS}
 )
 
 // AWT: layout/MainLayer
 @Reachable(condition = javax.imageio.ImageIO.class, jniAccessible = true,
-        classes = {java.awt.Insets.class, java.awt.Toolkit.class},
-        classNames = "sun.java2d.Disposer"
+    classes = {java.awt.Insets.class, java.awt.Toolkit.class},
+    classNames = "sun.java2d.Disposer"
 )
 @Reachable(condition = javax.imageio.ImageIO.class,
-        classes = {javax.imageio.spi.ImageReaderSpi.class, javax.imageio.spi.ImageWriterSpi.class},
-        memberAccess = {},
-        resources = "/META-INF/services/javax.imageio.spi.*",
-        bundles = "/sun.awt.resources.awt"
+    classes = {javax.imageio.spi.ImageReaderSpi.class, javax.imageio.spi.ImageWriterSpi.class},
+    memberAccess = {},
+    resources = "/META-INF/services/javax.imageio.spi.*",
+    bundles = "/sun.awt.resources.awt"
 )
 
 // AWT: filemanager/Utils
 @Reachable(condition = java.awt.Desktop.class, jniAccessible = true,
-        classes = {
-                java.awt.AWTEvent.class,
-                java.awt.AlphaComposite.class,
-                java.awt.Color.class,
-                java.awt.Component.class,
-                java.awt.Desktop.Action.class,
-                java.awt.Font.class,
-                java.awt.GraphicsEnvironment.class,
-                java.awt.Insets.class,
-                java.awt.Rectangle.class,
-                java.awt.Toolkit.class,
-                java.awt.desktop.UserSessionEvent.Reason.class,
-                java.awt.event.InputEvent.class,
-                java.awt.geom.AffineTransform.class,
-                java.awt.geom.Path2D.class,
-                java.awt.geom.Path2D.Float.class,
-                java.awt.image.ColorModel.class,
-                java.awt.image.IndexColorModel.class,
-                java.lang.System.class
-        },
-        classNames = {
-                "sun.awt.AWTAutoShutdown",
-                "sun.awt.SunHints",
-                "sun.awt.SunToolkit",
-                "sun.awt.X11.XDesktopPeer",
-                "sun.awt.X11.XErrorHandlerUtil",
-                "sun.awt.X11.XToolkit",
-                "sun.awt.image.SunVolatileImage",
-                "sun.awt.image.VolatileSurfaceManager",
-                "sun.awt.shell.Win32ShellFolderManager2",
-                "sun.awt.windows.WComponentPeer",
-                "sun.awt.windows.WDesktopPeer",
-                "sun.awt.windows.WObjectPeer",
-                "sun.awt.windows.WToolkit",
-                "sun.java2d.Disposer",
-                "sun.java2d.InvalidPipeException",
-                "sun.java2d.NullSurfaceData",
-                "sun.java2d.SunGraphics2D",
-                "sun.java2d.SurfaceData",
-                "sun.java2d.loops.Blit",
-                "sun.java2d.loops.BlitBg",
-                "sun.java2d.loops.CompositeType",
-                "sun.java2d.loops.DrawGlyphList",
-                "sun.java2d.loops.DrawGlyphListAA",
-                "sun.java2d.loops.DrawGlyphListLCD",
-                "sun.java2d.loops.DrawLine",
-                "sun.java2d.loops.DrawParallelogram",
-                "sun.java2d.loops.DrawPath",
-                "sun.java2d.loops.DrawPolygons",
-                "sun.java2d.loops.DrawRect",
-                "sun.java2d.loops.FillParallelogram",
-                "sun.java2d.loops.FillPath",
-                "sun.java2d.loops.FillRect",
-                "sun.java2d.loops.FillSpans",
-                "sun.java2d.loops.GraphicsPrimitive",
-                "sun.java2d.loops.GraphicsPrimitive[]",
-                "sun.java2d.loops.GraphicsPrimitiveMgr",
-                "sun.java2d.loops.MaskBlit",
-                "sun.java2d.loops.MaskFill",
-                "sun.java2d.loops.ScaledBlit",
-                "sun.java2d.loops.SurfaceType",
-                "sun.java2d.loops.TransformHelper",
-                "sun.java2d.loops.XORComposite",
-                "sun.java2d.marlin.DMarlinRenderingEngine",
-                "sun.java2d.pipe.Region",
-                "sun.java2d.pipe.RegionIterator",
-                "sun.java2d.xr.XRSurfaceData"
-        },
-        bundles = {"/sun.awt.resources.awt", "/sun.awt.resources.awtosx"}
+    classes = {
+        java.awt.AWTEvent.class,
+        java.awt.AlphaComposite.class,
+        java.awt.Color.class,
+        java.awt.Component.class,
+        java.awt.Desktop.Action.class,
+        java.awt.Font.class,
+        java.awt.GraphicsEnvironment.class,
+        java.awt.Insets.class,
+        java.awt.Rectangle.class,
+        java.awt.Toolkit.class,
+        java.awt.desktop.UserSessionEvent.Reason.class,
+        java.awt.event.InputEvent.class,
+        java.awt.geom.AffineTransform.class,
+        java.awt.geom.Path2D.class,
+        java.awt.geom.Path2D.Float.class,
+        java.awt.image.ColorModel.class,
+        java.awt.image.IndexColorModel.class,
+        java.lang.System.class
+    },
+    classNames = {
+        "sun.awt.AWTAutoShutdown",
+        "sun.awt.SunHints",
+        "sun.awt.SunToolkit",
+        "sun.awt.X11.XDesktopPeer",
+        "sun.awt.X11.XErrorHandlerUtil",
+        "sun.awt.X11.XToolkit",
+        "sun.awt.image.SunVolatileImage",
+        "sun.awt.image.VolatileSurfaceManager",
+        "sun.awt.shell.Win32ShellFolderManager2",
+        "sun.awt.windows.WComponentPeer",
+        "sun.awt.windows.WDesktopPeer",
+        "sun.awt.windows.WObjectPeer",
+        "sun.awt.windows.WToolkit",
+        "sun.java2d.Disposer",
+        "sun.java2d.InvalidPipeException",
+        "sun.java2d.NullSurfaceData",
+        "sun.java2d.SunGraphics2D",
+        "sun.java2d.SurfaceData",
+        "sun.java2d.loops.Blit",
+        "sun.java2d.loops.BlitBg",
+        "sun.java2d.loops.CompositeType",
+        "sun.java2d.loops.DrawGlyphList",
+        "sun.java2d.loops.DrawGlyphListAA",
+        "sun.java2d.loops.DrawGlyphListLCD",
+        "sun.java2d.loops.DrawLine",
+        "sun.java2d.loops.DrawParallelogram",
+        "sun.java2d.loops.DrawPath",
+        "sun.java2d.loops.DrawPolygons",
+        "sun.java2d.loops.DrawRect",
+        "sun.java2d.loops.FillParallelogram",
+        "sun.java2d.loops.FillPath",
+        "sun.java2d.loops.FillRect",
+        "sun.java2d.loops.FillSpans",
+        "sun.java2d.loops.GraphicsPrimitive",
+        "sun.java2d.loops.GraphicsPrimitive[]",
+        "sun.java2d.loops.GraphicsPrimitiveMgr",
+        "sun.java2d.loops.MaskBlit",
+        "sun.java2d.loops.MaskFill",
+        "sun.java2d.loops.ScaledBlit",
+        "sun.java2d.loops.SurfaceType",
+        "sun.java2d.loops.TransformHelper",
+        "sun.java2d.loops.XORComposite",
+        "sun.java2d.marlin.DMarlinRenderingEngine",
+        "sun.java2d.pipe.Region",
+        "sun.java2d.pipe.RegionIterator",
+        "sun.java2d.xr.XRSurfaceData"
+    },
+    bundles = {"/sun.awt.resources.awt", "/sun.awt.resources.awtosx"}
 )
 public class LibMetadata {
 }
